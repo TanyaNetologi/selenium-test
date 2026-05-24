211ebffdb908b9262b5b5e2e2e94bca53d3ebbd9
Neto test <tanyadima1909@gmail.com>
2026-05-24 18:25:03 +0500

    selenium

--- test_form.py
+++ test_form.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -14,31 +15,26 @@ def driver():
     yield driver
     driver.quit()
 
+@allure.title("Успешный вход в систему")
+@allure.description("Проверяем, что с правильными логином и паролем происходит вход и отображается сообщение об успехе")
+@allure.feature("Авторизация")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_successful_login(driver):
     driver.get("https://the-internet.herokuapp.com/login")
-    username = driver.find_element(By.ID, "username")
-    username.send_keys("tomsmith")
+    driver.find_element(By.ID, "username").send_keys("tomsmith")
+    driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    flash = driver.find_element(By.ID, "flash").text
+    assert "You logged into a secure area!" in flash
 
-    password = driver.find_element(By.ID, "password")
-    password.send_keys("SuperSecretPassword!")
-
-    login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-    login_button.click()
-
-    flash_message = driver.find_element(By.ID, "flash")
-    assert "You logged into a secure area!" in flash_message.text
-
+@allure.title("Неуспешный вход с неверными данными")
+@allure.description("Проверяем, что при вводе неправильных логина и пароля появляется сообщение об ошибке")
+@allure.feature("Авторизация")
+@allure.severity(allure.severity_level.NORMAL)
 def test_unsuccessful_login(driver):
     driver.get("https://the-internet.herokuapp.com/login")
-
-    username = driver.find_element(By.ID, "username")
-    username.send_keys("wrong_user")
-
-    password = driver.find_element(By.ID, "password")
-    password.send_keys("wrong_password")
-
-    login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-    login_button.click()
-
-    flash_message = driver.find_element(By.ID, "flash")
-    assert "Your username is invalid!" in flash_message.text
+    driver.find_element(By.ID, "username").send_keys("wrong_user")
+    driver.find_element(By.ID, "password").send_keys("wrong_password")
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    flash = driver.find_element(By.ID, "flash").text
+    assert "Your username is invalid!" in flash
